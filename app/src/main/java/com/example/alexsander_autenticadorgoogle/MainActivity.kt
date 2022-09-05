d7b9349c4a5b85f2002e2a93cac9eb1658795189
AlexsanderBenatti <abenatti.silva@gmail.com>
2022-09-05 08:41:01 -0300

    Felicidade :D

--- app/src/main/java/com/example/alexsander_autenticadorgoogle/MainActivity.kt
+++ app/src/main/java/com/example/alexsander_autenticadorgoogle/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -17,18 +18,20 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var imageView: ImageView
+    private lateinit var auth: FirebaseAuth
+    private lateinit var signInButton: com.google.android.gms.common.SignInButton
     private lateinit var client: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageView = findViewById(R.id.sign_in_button)
+        auth = FirebaseAuth.getInstance()
         val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         client = GoogleSignIn.getClient(this,options)
+        signInButton = findViewById(R.id.sign_in_button)
         var resultLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
@@ -48,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                     }
             }
         }
-        imageView.setOnClickListener{
+        signInButton.setOnClickListener{
             val intent = client.signInIntent
             resultLauncher.launch(intent)
         }
